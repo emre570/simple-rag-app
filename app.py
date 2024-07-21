fd18255f93947c87b39cc8b033b191fa0660126b
Cemal Emre Albayrak <70805503+emre570@users.noreply.github.com>
2024-07-21 15:05:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from document_handler import load_docs
 from indexer import split_docs
 from embedder import call_embed_model
@@ -6,24 +5,17 @@ from vector_db_handler import init_faiss
 from retriever import retrieve_docs
 from chain_handler import setup_chain
 
-#os.environ["LANGCHAIN_API_KEY"] = "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"
-
 data_folder = "C:\Developer\python\ml-works\llm-works/rag/rag_app\ollama-rag\data"
 docs = load_docs(data_folder)
 
 chunks = split_docs(docs)
-
-"""print(chunks)  # Bu, split_docs tarafından üretilen metin parçalarını göstermelidir.
-if not chunks:
-    print("Dökümanlar boş veya bölünemedi.")"""
-
 
 embed_model_name = "sentence-transformers/all-mpnet-base-v2"
 embeddings_model = call_embed_model(embed_model_name)
 
 vectorstore = init_faiss(chunks, embeddings_model)
 
-similar_docs_count = 5
+similar_docs_count = 8
 question = ""
 
 retriever = retrieve_docs(question, vectorstore, similar_docs_count, see_content=False)
